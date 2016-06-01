91fafdb10b924a3db03cf3dc4def90676a005149
Silas Mächler <silas.maechler@gmail.com>
2016-06-01 20:13:22 +0200

    test

---
+++ module.php
@@ -0,0 +1,12 @@
+<?
+
+class ForgeMailchimp extends Module {
+
+  public function setup() {
+    $this->name = i('Mailchimp for Forge', 'forge-news');
+    $this->description = i('Module to add newsletter Subscription form..');
+  }
+
+}
+
+?>
