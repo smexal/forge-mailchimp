98082016f637915d5d6a79585019e5679b8c8ce2
Silas Mächler <silas.maechler@gmail.com>
2016-09-27 21:39:10 +0200

    Delete form.8a586a3ba75dad7145a54ecd5fd2e46c.rtpl.php

--- cache/form.8a586a3ba75dad7145a54ecd5fd2e46c.rtpl.php
+++
@@ -1,6 +0,0 @@
-<?php if(!class_exists('raintpl')){exit;}?><div class="forge-mailchimp-form">
-    <p class="feddow"><?php echo $before;?></p>
-    <form class="ajax" action="<?php echo $action;?>" callback="forgeMailchimp.formCallback">
-    <?php echo $form;?>
-    </form>
-</div>
